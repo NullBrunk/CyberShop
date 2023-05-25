dd056f97785e6a069828726bfe65eeb20f80ec32
Brunk <nullbrunk@proton.me>
2023-05-25 17:26:28 +0200

    Refactoring

--- resources/views/static/details.blade.php
+++ resources/views/static/details.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Details</title>
+  <title>{{$data["name"]}}</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
