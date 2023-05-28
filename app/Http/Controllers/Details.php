6e5070451388fd9843a6f9fdbc7e55a246e62c74
Brunk <nullbrunk@proton.me>
2023-05-28 22:55:26 +0200

    Replacing  by Request

--- app/Http/Controllers/Details.php
+++ app/Http/Controllers/Details.php
@@ -2,18 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class Details extends Controller
 {
-    public function __invoke(){
+    public function __invoke(Request $request){
         
         include_once __DIR__ . '/../Database/config.php';
         
-        
-        $r = $pdo -> prepare("SELECT users.id as uid, product.id as pid, id_user, price, descr, class, mail, name FROM product LEFT JOIN users ON users.id=product.id_user WHERE product.id=:id");
-        $r -> execute( [ "id" => $_GET['id'] ] );
-        $r = $r -> fetch();
+        $id = $request -> input("id");
 
-        return view("static.details", ["data" => $r]);
+        if($id){
+            $r = $pdo -> prepare("SELECT users.id as uid, product.id as pid, id_user, price, descr, class, mail, name FROM product LEFT JOIN users ON users.id=product.id_user WHERE product.id=:id");
+            $r -> execute( [ "id" => $id ] );
+            $r = $r -> fetch();
+            
+            if($r){
+                return view("static.details", ["data" => $r]);
+            }
+            else {
+                abort(403);
+            }
+        }
+        else {
+            abort(403);
+        }
+        
+
 
     }
 }
