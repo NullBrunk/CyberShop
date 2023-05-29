d32bf24ad523ed38abd494c8280fb65ded39cd7e
Brunk <nullbrunk@proton.me>
2023-05-29 16:30:17 +0200

    Changed title of login page

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -60,7 +60,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>WebChat</title>
+  <title>E-Commerce</title>
   <meta content="" name="description">
 
   <meta content="" name="keywords">
