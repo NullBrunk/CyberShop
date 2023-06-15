b02cfa293c440587bc0cffbe49ea20eab86d4226
Brunk <nullbrunk@proton.me>
2023-06-15 20:46:06 +0200

    Addedd profile page

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -125,42 +125,25 @@
 
                 <hr class="my-4">
                 <!-- Address -->
-                <h6 class="heading-small text-muted mb-4">Contact information</h6>
+                <h6 class="heading-small text-muted mb-4">Buyed products</h6>
+                <²div class="pl-lg-4">
+                  <div class="row">
+                    
+                  </div>
+          
+                </div>
+
+                <hr class="my-4">
+
+                <!-- Description -->
+
+                <h6 class="heading-small text-muted mb-4">Selled products</h6>
                 <div class="pl-lg-4">
                   <div class="row">
                     
                   </div>
-                  <div class="row">
-                    <div class="col-lg-4">
-                      <div class="form-group focused">
-                        <label class="form-control-label" for="input-city">City</label>
-                        <input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York">
-                      </div>
-                    </div>
-                    <div class="col-lg-4">
-                      <div class="form-group focused">
-                        <label class="form-control-label" for="input-country">Country</label>
-                        <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
-                      </div>
-                    </div>
-                    <div class="col-lg-4">
-                      <div class="form-group">
-                        <label class="form-control-label" for="input-country">Postal code</label>
-                        <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
-                      </div>
-                    </div>
-                  </div>
                 </div>
-                <hr class="my-4">
-                <!-- Description -->
-                <h6 class="heading-small text-muted mb-4">About me</h6>
-                <div class="pl-lg-4">
-                  <div class="form-group focused">
-                    <label>About Me</label>
-                    <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
-                  </div>
-                </div>
-              </form>
+
             </div>
           </div>
         </div>
