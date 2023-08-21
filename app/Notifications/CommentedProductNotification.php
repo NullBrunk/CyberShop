3230bb98384eb9c361837cff09fb47a3f1f245fe
NullBrunk <nullbrunk@proton.me>
2023-08-21 16:44:14 +0200

    Small optimization of notifications

--- app/Notifications/CommentedProductNotification.php
+++ app/Notifications/CommentedProductNotification.php
@@ -15,9 +15,7 @@ class CommentedProductNotification extends Notification
      * Create a new notification instance.
      */
     public function __construct(public array $data)
-    {
-        //
-    }
+    {}
 
     /**
      * Get the notification's delivery channels.
@@ -27,17 +25,6 @@ class CommentedProductNotification extends Notification
     public function via(object $notifiable): array
     {
         return ['database'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
     }
 
     /**
--- app/Notifications/ReceivedMessageNotification.php
+++ app/Notifications/ReceivedMessageNotification.php
@@ -2,10 +2,8 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Bus\Queueable;
 
 class ReceivedMessageNotification extends Notification
 {
@@ -15,9 +13,7 @@ class ReceivedMessageNotification extends Notification
      * Create a new notification instance.
      */
     public function __construct(public array $data)
-    {
-        //
-    }
+    {}
 
     /**
      * Get the notification's delivery channels.
@@ -27,17 +23,6 @@ class ReceivedMessageNotification extends Notification
     public function via(object $notifiable): array
     {
         return ['database'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
     }
 
     /**
