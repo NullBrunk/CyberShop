092fc68f4312319e03ddcfa9484e7d7c89164445
Anas <125673909+NullBrunk@users.noreply.github.com>
2024-04-15 21:24:05 +0200

    Update wait-for-mysql.sh

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-until mysqladmin ping -h mysql -P 3306 --silent; do
+until mariadb-admin ping -h mysql -P 3306 --silent; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
